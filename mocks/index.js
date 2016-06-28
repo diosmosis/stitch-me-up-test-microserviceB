b6e1bbeade64159e6256d222ff99ed8f17eb302e
Benaka Moorthi <benaka.moorthi@mheducation.com>
2016-06-28 11:52:41 -0700

    Initial commit.

---
+++ mocks/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var app = require('koa')()
+var router = require('koa-router')()
+
+router.get('/service-b', function* (next) {
+  this.body = 'mock-serviceb-response'
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(3000)
